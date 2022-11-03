23506a7493974cbeedd9f4ea792a3e4c1904963a
Konstantin_Ermolaev <64197341+ThaHatty-KonstantinErmolaev@users.noreply.github.com>
2022-11-03 13:05:28 +0700

    first commit

---
+++ database/migrations/2022_11_03_042508_create_journal_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJournalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journal', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('book_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status');
+            $table->boolean('is_digital');
+            $table->string('book_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journal');
+    }
+}
